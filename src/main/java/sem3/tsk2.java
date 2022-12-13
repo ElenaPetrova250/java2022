f4a878ed579d3d2c3f3642bbed3e3fade8091581
Elena Petrova <elena.i.11@yandex.ru>
2022-12-13 21:54:28 +0400

    git remote add origin git@github.com:ElenaPetrova250/java2022.git git branch -M main git push -u origin main

---
+++ src/main/java/sem3/tsk2.java
@@ -0,0 +1,16 @@
+package sem3;
+import java.util.ArrayList;
+
+public class tsk2 {
+
+    public class Main {
+        System.out.println(Collections.max(ar));
+        System.out.println(Collections.min(ar));
+        public static void main(String[] args) {
+            ArrayList<Integer> ar = new ArrayList<>();
+            ar.add(12);
+            ar.add(21);
+            ar.add(111);
+        }
+    }
+}
